02917164b82c8ab8476c6a53d0e919d6ebde0d45
king <923789900@qq.com>
2020-01-12 15:38:20 +0800

    update

--- app/src/main/java/com/king/httpservertest/logUtils/SocketUtils/SocketListener.java
+++ app/src/main/java/com/king/httpservertest/logUtils/SocketUtils/SocketListener.java
@@ -4,11 +4,11 @@ import java.net.Socket;
 
 public interface SocketListener {
 
-    public abstract void event_Connection(Socket client);
+    public abstract void event_Connection(ClientSocket client);
 
-    public abstract void event_Recevied(Socket client,byte[] Data,int Length);
+    public abstract void event_Recevied(ClientSocket client,byte[] Data,int Length);
 
-    public abstract void event_disConnection(Socket client);
+    public abstract void event_disConnection(ClientSocket client);
 
     
 }
